77ad3adc05d82c88fc01cc7f8e7fa02e35f008dc
Hieu Nguyen <hieunguyen1053@outlook.com>
2021-06-13 13:59:19 +0700

    update hmm/evaluate.py

--- hmm/evaluate.py
+++ hmm/evaluate.py
@@ -4,39 +4,40 @@ from tqdm import tqdm
 from hmm import HMM_NER
 
 
-def create_tags(sentences):
+def _get_tags(sents):
     tags = []
-    for sentence_index, iob_tags in enumerate(sentences):
-        i = 0
-        current_tag = [None, None, None, None]
-        while i < len(iob_tags):
-            if iob_tags[i] == "O" and current_tag[0]:
-                tags.append(tuple(current_tag))
-                current_tag = [None, None, None, None]
-            elif iob_tags[i].startswith("B"):
-                current_tag[0] = iob_tags[i][2:]
-                current_tag[1] = i
-                current_tag[2] = i
-                current_tag[3] = sentence_index
-            elif iob_tags[i].startswith("I"):
-                current_tag[2] = i
-            i += 1
-        if current_tag[0]:
-            tags.append(tuple(current_tag))
+    for sent_idx, iob_tags in enumerate(sents):
+        curr_tag = {'type': None, 'start_idx': None,
+                    'end_idx': None, 'sent_idx': None}
+        for i, tag in enumerate(iob_tags):
+            if tag == 'O' and curr_tag['type']:
+                tags.append(tuple(curr_tag.values()))
+                curr_tag = {'type': None, 'start_idx': None,
+                            'end_idx': None, 'sent_idx': None}
+            elif tag.startswith('B'):
+                curr_tag['type'] = tag[2:]
+                curr_tag['start_idx'] = i
+                curr_tag['end_idx'] = i
+                curr_tag['sent_idx'] = sent_idx
+            elif tag.startswith('I'):
+                curr_tag['end_idx'] = i
+        if curr_tag['type']:
+            tags.append(tuple(curr_tag.values()))
     tags = set(tags)
     return tags
 
 
-def iob_score(y_true, y_pred):
-    tags_true = create_tags(y_true)
-    tags_pred = create_tags(y_pred)
-    ref = len(tags_true)
-    true = len(set(tags_true).intersection(tags_pred))
-    sys = len(tags_pred)
-    if ref == 0 or true == 0 or sys == 0:
+def f_measure(y_true, y_pred):
+    tags_true = _get_tags(y_true)
+    tags_pred = _get_tags(y_pred)
+
+    ne_ref = len(tags_true)
+    ne_true = len(set(tags_true).intersection(tags_pred))
+    ne_sys = len(tags_pred)
+    if ne_ref == 0 or ne_true == 0 or ne_sys == 0:
         return 0
-    p = true / sys
-    r = true / ref
+    p = ne_true / ne_sys
+    r = ne_true / ne_ref
     f1 = (2 * p * r) / (p + r)
 
     return f1
@@ -57,11 +58,8 @@ def evaluate(model, filepath):
             y.append(n)
         y_pred.append(model.viterbi(x))
         y_test.append(y)
-        print(y_pred[-1])
-        print(y_test[-1])
 
-    # print(classification_report(y_test, y_pred, labels=['B-LOC', 'I-LOC', 'B-MISC', 'I-MISC', 'B-ORG', 'I-ORG', 'B-PER', 'I-PER', 'O']))
-    print(iob_score(y_test, y_pred))
+    return f_measure(y_test, y_pred)
 
 
 if __name__ == '__main__':
@@ -71,4 +69,5 @@ if __name__ == '__main__':
     emiss = params['emiss']
     trans = params['trans']
     model = HMM_NER(vocab, label, emiss, trans)
-    evaluate(model, '/Users/hieunguyen/Desktop/NLP/Master/NER/data/vlsp2016/test.txt')
+    f1_score = evaluate(model, '/Users/hieunguyen/Desktop/NLP/Master/NER/data/vlsp2016/test.txt')
+    print(f1_score)
